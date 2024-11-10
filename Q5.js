c83b0d8f43641f4366ecbd28d3753f065b409a1e
Novish Khalil <novishkhalil2@hotmail.com>
2024-11-11 00:03:24 +0500

    Add files via upload

---
+++ Q5.js
@@ -0,0 +1,5 @@
+"use strict";
+//5..Famous Quote 2: Repeat Q-2, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+let famous_person = "Hellen Keller";
+let message = "“Alone we can do so little; together we can do so much.” ";
+console.log(` ${famous_person} said: ${message}`);
---
+++ Q5.ts
@@ -0,0 +1,6 @@
+//5..Famous Quote 2: Repeat Q-2, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+let famous_person:string = "Hellen Keller"
+
+let message:string ="“Alone we can do so little; together we can do so much.” "
+
+console.log(` ${famous_person} said: ${message}`)
